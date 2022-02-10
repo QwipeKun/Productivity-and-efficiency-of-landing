6beb3b6aa04a9d88b907f531b223015052621660
QwipeKun <moonsov@yandex.ru>
2022-02-10 20:34:32 +0300

    Component design

--- src/components/MainContent/CentralContent/CentralContent.jsx
+++ src/components/MainContent/CentralContent/CentralContent.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import s from "./CentralContent.module.css";
 import picWoman from "./../../../image/designer/undraw_mobile_login_ikmv.svg"
+import icon from "./../../../image/designer/Icon.svg"
 
 let CentralContent = (props) => {
   return (
+      <div>
     <div className={s.centralContentContainer}>
       <div>
         <div className={s.textBlockContainer}>
@@ -22,19 +24,20 @@ let CentralContent = (props) => {
             </p>
           </div>
           <div className={s.blocksUnderTheText}>
-            <div>
-              <div><img src="" alt="" /></div>
-              <div>Title Goes Here</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </div>
+            <div className={s.titleChildBlock}>
+              <div className={s.imageIcon}><img src={icon} alt="" /></div>
+              <div className={s.titleBlock}>Title Goes Here</div>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
             </div>
-            <div>
-              <div><img src="" alt="" /></div>
-              <div>Title Goes Here</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </div>
+            <div className={s.titleChildBlock}>
+              <div  className={s.imageIcon}><img src={icon} alt="" /></div>
+              <div className={s.titleBlock}>Title Goes Here</div>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
       <div className={s.picWomanImage}>
         <img src={picWoman} alt="wooman" />
       </div>
